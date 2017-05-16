967f2823ed023e128bd35b90dca8d0ecca64c154
Iago Lourenço <iagojlourenco@gmail.com>
2017-05-16 09:00:59 -0300

    Create 1006.cpp

---
+++ 1006.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+	double a, b, c, media;
+	scanf("%lf",&a);
+	scanf("%lf", &b);
+	scanf("%lf", &c);
+	media = ((a*2) + (b*3) + (c*5))/10;
+	printf("MEDIA = %.1lf\n", media);
+	system("pause");
+}
