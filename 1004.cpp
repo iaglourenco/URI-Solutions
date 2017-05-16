12daf411ae785f1594f329aeb8cce21afd6203f9
Iago Lourenço <iagojlourenco@gmail.com>
2017-05-16 08:59:48 -0300

    Create 1004.cpp

---
+++ 1004.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+	int a, b, prod;
+	scanf("%i", &a);
+	scanf("%i", &b);
+	prod = a*b;
+	printf("PROD = %i\n", prod);
+	system("pause");
+}
