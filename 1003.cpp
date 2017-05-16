04a0d7727789dc9aa2a5fe073cb9d2bdf2b07ef9
Iago Lourenço <iagojlourenco@gmail.com>
2017-05-16 08:59:24 -0300

    Create 1003.cpp

---
+++ 1003.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+	int a, b, soma;
+	//printf("digite a");
+	scanf("%i", &a);
+	//printf("digite b");
+	scanf("%i", &b);
+	soma = a + b;
+	printf("SOMA = %i\n",soma);
+	system("pause");
+}
